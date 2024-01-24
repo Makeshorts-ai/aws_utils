ee7b138b8fe857b91e2d1fc440bad0f4fb0eb517
Atul Shukla <atul@kollabia.com>
2024-01-24 12:04:53 +0530

    instance id , ec2 module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws_utils',   # The name of your package
-    version='0.1.1',        # The version of your package
+    version='0.1.4',        # The version of your package
     packages=find_packages(),  # Automatically discover and include all packages in your project
 
     install_requires=[
